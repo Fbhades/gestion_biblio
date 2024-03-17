a8bbbdadfcb101c03dc66e0e8723fae6bfe88065
Klali-Firas <firasklali1234@gmail.com>
2024-03-17 17:36:55 +0100

    Made Books page public

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/', '/api/books', '/api/books/:id(\\d+)', '/api/auth', '/api/categories', '/api/categories/:id(\\d+)'],
+  publicRoutes: ['/', '/api/books', '/api/books/:id(\\d+)', '/api/auth', '/api/categories', '/api/categories/:id(\\d+)', '/books'],
 });
 
 export const config = {
