071df8448dde38e7ba44f2b48649ea615006ea69
Sarah Haj Yahia <ssarahjys@gmail.com>
2024-05-11 09:27:25 +0100

    interface +

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import { FaSearch } from 'react-icons/fa';
 import { UserButton } from "@clerk/clerk-react";
 import Navbar from "./Ui/Navbar";
 import { handleCreateUser } from "@/components/user";
+import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
+
+
 
 
 export default function Home() {
@@ -12,7 +16,7 @@ export default function Home() {
   const { isLoaded, userId, sessionId, getToken } = useAuth();
   const handleCreateUser = async () => {
     try {
-       const { isLoaded, isSignedIn, user } = useUser();
+      const { isLoaded, isSignedIn, user } = useUser();
       const email = user?.emailAddresses[0].toString();
       const firstname = user?.firstName?.toString();
       const lastname  = user?.lastName?.toString();
@@ -33,6 +37,13 @@ export default function Home() {
      }
    };
   if (userId) { handleCreateUser() }
+
+  // const router = useRouter();
+  // const handleEmpruntsClick = () => {
+  //   router.push('/loans');
+
+  
+
   return (
     <div>
       <Navbar />
@@ -47,13 +58,20 @@ export default function Home() {
           <h1 className="text-5xl text-white font-bold mb-8">A place to learn, grow, and explore</h1>
         </section>
 
-        {/* À propos Section */}
-        <section id="a-propos" className="bg-gray-200  py-60 mb-8 "> {/* Increased padding-y */}
-          <div className="max-w-screen-full mx-auto text-center">
-            <h1 className="text-3xl font-bold text-black  mb-4">À propos</h1>
-            <p className="text-lg text-gray-800 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut lectus vitae magna luctus blandit. Nullam pulvinar dui sed interdum sagittis.</p>
+      {/* à propos Section */}
+      <section id="a-propos" className="py-16 bg-gray-200">
+        <div className="container mx-auto px-4 flex justify-center"><br /><br />
+          <div className="max-w-screen-md"><br /><br /><br /><br /><br /><br />
+            <h1 className="text-3xl font-bold text-center text-black mb-4">À propos</h1>
+            <p className="text-lg text-gray-800 text-center">
+              L’Institut supérieur des études technologiques de Radès (arabe : المعاهد العليا للدراسات التكنولوجية برادس) (ISET-Radès) est une institution d'enseignement supérieur tunisienne formant des techniciens supérieurs et délivrant le diplôme de licence appliquée dans plusieurs domaines. <br />
+              Il assure la formation selon la réforme LMD du système de l'enseignement supérieur tunisien au sein des départements suivants : génie mécanique, génie civil, génie électrique, technologies d'informatique et gestion des entreprises et des administrations. <br />
+              L'Iset de Radès est situé dans la banlieue de Radès (gouvernorat de Ben Arous) et abrite aussi la direction générale des études technologiques2.
+            </p>
           </div>
-        </section>
+        </div>
+      </section>
+
 
         {/* Nos services Section */}
         <section id="nos-services" className="bg-gray-200  py-60 mb-8 ">
@@ -61,31 +79,62 @@ export default function Home() {
             <h1 className="text-3xl font-bold text-black  mb-4">Nos services</h1>
             <div className="flex justify-center space-x-8">
               {/* Service Box 1 */}
-              <div className="bg-white  rounded-lg shadow-lg p-6 flex-1">
-                <h2 className="text-xl font-bold text-gray-800  mb-4">Nos Evènements</h2>
-                <p className="text-gray-600 ">Nos eveènements seront bientot affichés</p>
+              <div className="bg-white rounded-lg shadow-lg p-6 flex-1 flex flex-col items-center justify-center">
+                <img src="reservation.png" alt="Reservation Espace" className="mb-4" />
+                <h2 className="text-xl font-bold text-gray-800 mb-2">Réservation Espace</h2>
+                <p className="text-gray-600">Réservez un espace pour une expérience exceptionnelle</p>
               </div>
+
               {/* Service Box 2 */}
-              <div className="bg-white  rounded-lg shadow-lg p-6 flex-1">
-                <h2 className="text-xl font-bold text-gray-800  mb-4">Réeservation Espace</h2>
-                <p className="text-gray-600 ">Réservez un espace our une expérience exceptionnelle</p>
+              <div className="bg-white rounded-lg shadow-lg p-6 flex-1 flex flex-col items-center justify-center">
+                <img src="books.png" alt="Réservation Livres" className="mb-4" />
+                <h2 className="text-xl font-bold text-gray-800 mb-2">Réservation Livres</h2>
+                <p className="text-gray-600">Réservez des livres et profitez de nouvelles connaissances</p>
               </div>
+
               {/* Service Box 3 */}
-              <div className="bg-white  rounded-lg shadow-lg p-6 flex-1">
-                <h2 className="text-xl font-bold text-gray-800  mb-4">Emprunt livre</h2>
-                <p className="text-gray-600 ">Veuillez nous rejoindre et empruntez des livre inconditionnellement</p>
+              <div className="bg-white rounded-lg shadow-lg p-6 flex-1 flex flex-col items-center justify-center">
+                <img src="loans.png" alt="Vos Emprunts" className="mb-4" />
+                <h2 className="text-xl font-bold text-gray-800 mb-2">Vos Emprunts</h2>
+                <p className="text-gray-600">Suivez vos emprunts</p>
               </div>
             </div>
           </div>
         </section>
 
         {/* Nouveautés Section */}
-        <section id="nouveautes" className="bg-gray-200  py-60 mb-8">
-          <div className="max-w-screen-full mx-auto text-center">
-            <h1 className="text-3xl font-bold text-black  mb-4">Nouveautés</h1>
-            <p className="text-lg text-gray-800 ">Nos nouveautés seront affichés ici</p>
+        <section id="nouveautes" className="bg-gray-200 py-8">
+          <div className="container mx-auto px-4">
+          <h1 className="text-3xl font-bold text-center text-black mb-4">Nouveautés</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              {/* Première boîte */}
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <img src="club.png" alt="Image 1" className="w-full h-auto" />
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold text-gray-800 mb-2  h-16">Nos Clubs</h2>
+                  <p className="text-sm text-gray-600">Veuillez rejoindre nos clubs universitaires et participer aux exculisvités</p>
+                </div>
+              </div>
+              {/* Deuxième boîte */}
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <img src="events.png" alt="Image 2" className="w-full h-auto" />
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold text-gray-800 mb-2  h-16">Nos Evènements</h2>
+                  <p className="text-sm text-gray-600">Explorez nos évènements du campus et et profitez de nos evènements exlusifs</p>
+                </div>
+              </div>
+              {/* Troisième boîte */}
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <img src="partnership.png" alt="Image 3" className="w-full h-auto" />
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold text-gray-800 mb-2  h-16">Nos Partenaires</h2>
+                  <p className="text-sm text-gray-600">Suivez nos Partenaires et Sponsors pour notre bibliothèque</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
+
 
         {/* Contact Section */}
         <section id="contact" className="bg-gray-200  py-60 mb-8">
